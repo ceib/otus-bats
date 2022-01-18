c34368a226b5a1c7cfc5bd4306d0d14465c8c753
Aleksey Vorontsov <avorontsov1987@gmail.com>
2022-01-18 19:50:32 +0300

    - Реализованы компоненты тайп класса Show - Реализованы необходимые компоненты Monad
    Signed-off-by: Aleksey Vorontsov <avorontsov1987@gmail.com>

--- src/main/scala/me.chuwy/otusbats/Monad.scala
+++ src/main/scala/me.chuwy/otusbats/Monad.scala
@@ -5,9 +5,59 @@ trait Monad[F[_]] extends Functor[F] { self =>
 
   def point[A](a: A): F[A]
 
-  def flatten[A](fa: F[F[A]]): F[A]
+  def flatten[A](fa: F[F[A]]): F[A] = flatMap(fa)(a => a)
+
+  /** Комбинатор в трейте */
+  def zip[A, B](fa: F[A], fb: F[B]): F[(A, B)] = flatMap(fa)(a => map(fb)((a, _)))
 }
 
 object Monad {
+
+  /** Суммонер */
+  def apply[F[_]](implicit ev: Monad[F]): Monad[F] = ev
+
+  /** Инстансы */
+  implicit val optionMonad: Monad[Option] = new Monad[Option] {
+    override def flatMap[A, B](fa: Option[A])(f: A => Option[B]): Option[B] = fa.flatMap(f)
+
+    override def point[A](a: A): Option[A] = Option(a)
+
+    override def map[A, B](fa: Option[A])(f: A => B): Option[B] = fa.map(f)
+  }
+
+  implicit val listMonad: Monad[List] = new Monad[List] {
+    override def flatMap[A, B](fa: List[A])(f: A => List[B]): List[B] = fa.flatMap(f)
+
+    override def point[A](a: A): List[A] = List(a)
+
+    override def map[A, B](fa: List[A])(f: A => B): List[B] = fa.map(f)
+  }
+
+  type E[A] = Either[_, A]
+  implicit val eitherMonad: Monad[E] = new Monad[E] {
+    override def flatMap[A, B](fa: E[A])(f: A => E[B]): E[B] = fa match {
+      case Left(value) => Left(value)
+      case Right(value) => f(value)
+    }
+
+    override def point[A](a: A): E[A] = Right(a)
+
+    override def map[A, B](fa: E[A])(f: A => B): E[B] = fa match {
+      case Left(value) => Left(value)
+      case Right(value) => Right(f(value))
+    }
+  }
+
+  /** Синтаксис */
+  implicit class MonadOps[F[_], A](fa: F[A]) {
+    def map[B](f: A => B)(implicit ev: Monad[F]): F[B] = ev.map(fa)(f)
+    def flatMap[B](f: A => F[B])(implicit ev: Monad[F]): F[B] = ev.flatMap(fa)(f)
+    def point[A](a: A)(implicit ev: Monad[F]): F[A] = ev.point(a)
+    def zip[B](fb: F[B])(implicit ev: Monad[F]): F[(A, B)] = ev.zip(fa, fb)
+  }
+
+  /** Комбинатор в объекте */
+  def nested[F[_], G[_], A, B](f: A => B)(fga: F[G[A]])(implicit monadF: Monad[F], monadG: Monad[G]): F[G[B]] =
+    fga.map(ga => ga.map(f))
 
 }
--- src/main/scala/me.chuwy/otusbats/Show.scala
+++ src/main/scala/me.chuwy/otusbats/Show.scala
@@ -1,6 +1,5 @@
 package me.chuwy.otusbats
 
-
 trait Show[A] {
   def show(a: A): String
 }
@@ -8,21 +7,25 @@ trait Show[A] {
 object Show {
 
   // 1.1 Instances (`Int`, `String`, `Boolean`)
+  implicit val intShow: Show[Int] = fromJvm[Int]
 
+  implicit val stringShow: Show[String] = fromJvm[String]
+
+  implicit val booleanShow: Show[Boolean] = fromJvm[Boolean]
 
   // 1.2 Instances with conditional implicit
 
-  implicit def listShow[A](implicit ev: Show[A]): Show[List[A]] =
-    ???
+  implicit def listShow[A](implicit ev: Show[A]): Show[List[A]] = fromFunction[List[A]](_.map(_.show).mkString(","))
 
 
   // 2. Summoner (apply)
+  def apply[A](implicit ev: Show[A]): Show[A] = ev
 
   // 3. Syntax extensions
 
   implicit class ShowOps[A](a: A) {
     def show(implicit ev: Show[A]): String =
-      ???
+      ev.show(a)
 
     /** Transform list of `A` into `String` with custom separator, beginning and ending.
      *  For example: "[a, b, c]" from `List("a", "b", "c")`
@@ -32,16 +35,18 @@ object Show {
      *  @param end. ']' in above example
      */
     def mkString_(list: List[A], separator: String, begin: String, end: String)(implicit ev: Show[A]): String =
-      ???
+      begin + list.map(_.show).mkString(separator) + end
 
   }
 
   // 4. Helper constructors
 
   /** Just use JVM `toString` implementation, available on every object */
-  def fromJvm[A]: Show[A] = ???
+  def fromJvm[A]: Show[A] = fromFunction(_.toString)
   
   /** Provide a custom function to avoid `new Show { ... }` machinery */
-  def fromFunction[A](f: A => String): Show[A] = ???
+  def fromFunction[A](f: A => String): Show[A] = new Show[A] {
+    override def show(a: A): String = f(a)
+  }
 
 }
